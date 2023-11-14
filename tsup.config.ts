28b7ed12a1bf7cd8e1744adef6e16e6f8a7dce8c
Kirill Machekhin <kamachekhin@avito.ru>
2023-11-14 16:19:36 +0700

    tsup build improvements

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+    outDir: 'dist',
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
     splitting: false,
     sourcemap: false,
     clean: true,
-    dts: true,
+    treeshake: true,
     minify: true,
+    platform: 'neutral',
+    dts: true,
 });
