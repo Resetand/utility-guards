cd066571997436355bc36468eea426b6b7441abf
Kirill Machekhin <kamachekhin@avito.ru>
2023-11-21 23:56:50 +0700

    fix ussue

--- scripts/postpack.js
+++ scripts/postpack.js
@@ -4,7 +4,7 @@ const { restoreCurrentChanges, execGit } = require('./_shared');
 async function main() {
     // discard all changes after prepack
     console.debug('Discarding all changes after prepack...');
-    await execGit('git reset --hard');
+    await execGit('reset --hard');
 
     console.debug('Restoring current changes from git stash...');
     // restore changes before prepack if any
