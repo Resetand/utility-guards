1e6c068ff894d971c15dbde672cf2f15dc37ffd3
Kirill Machekhin <kamachekhin@avito.ru>
2023-11-22 00:10:19 +0700

    fix

--- scripts/postpack.js
+++ scripts/postpack.js
@@ -4,7 +4,8 @@ const { restoreCurrentChanges, execGit } = require('./_shared');
 async function main() {
     // discard all changes after prepack
     console.log('Discarding all changes after prepack...');
-    await execGit('clean -f -d');
+    await execGit('reset --hard');
+    await execGit('clean -fd');
 
     console.log('Restoring current changes from git stash...');
     // restore changes before prepack if any
